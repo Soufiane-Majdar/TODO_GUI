8c5ec7e2bf63d9bc0479e409b9166eaa5d321564
Soufiane-Majdar <soufianemjr3145@gmail.com>
2023-01-08 21:19:44 +0100

    update Todo Frame stayle

--- src/TodoFrame.java
+++ src/TodoFrame.java
@@ -35,20 +35,45 @@ public class TodoFrame extends JFrame {
         txt_Task.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         txt_Task.setHorizontalAlignment(JTextField.CENTER);
     
-        txt_Task.setBounds(10,80,350,25);
+        txt_Task.setBounds(10,50,350,25);
 
 
         btn_Add = new JButton("Add");
-        btn_Add.setBounds(10,40,80,25);
+        // style
+        btn_Add.setBackground(new Color(47, 52, 63));
+        btn_Add.setForeground(Color.white);
+        btn_Add.setFont(new Font("Arial", Font.BOLD, 12));
+        btn_Add.setBorder(BorderFactory.createLineBorder(new Color(90, 97, 112)));
+        btn_Add.setBounds(10,10,80,25);
         btn_Edit = new JButton("Edit");
-        btn_Edit.setBounds(100,40,80,25);
+        // style
+        btn_Edit.setBackground(new Color(47, 52, 63));
+        btn_Edit.setForeground(Color.white);
+        btn_Edit.setFont(new Font("Arial", Font.BOLD, 12));
+        btn_Edit.setBorder(BorderFactory.createLineBorder(new Color(90, 97, 112)));
+        btn_Edit.setBounds(100,10,80,25);
         btn_Delete = new JButton("Delete");
-        btn_Delete.setBounds(190,40,80,25);
+        // style
+        btn_Delete.setBackground(new Color(47, 52, 63));
+        btn_Delete.setForeground(Color.white);
+        btn_Delete.setFont(new Font("Arial", Font.BOLD, 12));
+        btn_Delete.setBorder(BorderFactory.createLineBorder(new Color(90, 97, 112)));
+        btn_Delete.setBounds(190,10,80,25);
         btn_Update = new JButton("Update");
-        btn_Update.setBounds(280,40,80,25);
+        // style
+        btn_Update.setBackground(new Color(47, 52, 63));
+        btn_Update.setForeground(Color.white);
+        btn_Update.setFont(new Font("Arial", Font.BOLD, 12));
+        btn_Update.setBorder(BorderFactory.createLineBorder(new Color(90, 97, 112)));
+        btn_Update.setBounds(280,10,80,25);
 
         btn_Search = new JButton("Search");
-        btn_Search.setBounds(10,120,100,25);
+        // style
+        btn_Search.setBackground(new Color(90, 97, 112));
+        btn_Search.setForeground(Color.white);
+        btn_Search.setFont(new Font("Arial", Font.BOLD, 12));
+        btn_Search.setBorder(BorderFactory.createLineBorder(new Color(115, 122, 136)));
+        btn_Search.setBounds(10,90,100,25);
 
 
         lst_Tasks = new JList();
@@ -56,11 +81,11 @@ public class TodoFrame extends JFrame {
         lst_Tasks.setForeground(Color.gray);
         lst_Tasks.setFont(new Font("Arial", Font.ITALIC, 12));
         lst_Tasks.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        lst_Tasks.setBounds(10,160,350,250);
+        lst_Tasks.setBounds(10,130,350,270);
 
         // panel
         panel = new JPanel();
-        panel.setBackground(Color.gray);
+        panel.setBackground(new Color(47, 52, 63));
         panel.setLayout(null);
       
 
@@ -80,7 +105,10 @@ public class TodoFrame extends JFrame {
      
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Dimention
         this.setSize(380, 450);
+        this.setResizable(false);
         this.setVisible(true);
     }
 
